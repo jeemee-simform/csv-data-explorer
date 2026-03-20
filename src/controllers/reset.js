394e3bb6b4b991e19a201fa168bbc1e11498a13a
jeemee-simform <jeemee.sakariya@simformsolutions.com>
2026-03-20 14:52:00 +0530

    reset functionality added

--- src/controllers/reset.js
+++ src/controllers/reset.js
@@ -1,3 +1,37 @@
-const handleResetBtn = () => {};
+import { renderUi } from "../services/renderComponent.js";
+import { CONSTANTS } from "../utils/constants.js";
+import { getStates, setStates } from "../utils/helper.js";
+
+const handleResetBtn = () => {
+  try {
+    const states = getStates();
+
+    const { originalData: data } = states;
+
+    states.filteredData = [...data];
+    states.pagination = {
+      pageNumber: 1,
+      pageSize: CONSTANTS.DEFAULT_PAGE_SIZE,
+      persistIndex: 0,
+      totalPage: Math.ceil(data.length / CONSTANTS.DEFAULT_PAGE_SIZE),
+    };
+    states.sort = {
+      sortBy: null,
+      sortType: null,
+      cellIndex: null,
+    };
+    states.search = {
+      searchBy: "all",
+      searchText: "",
+    };
+    states.selectedRows = [];
+
+    setStates(states);
+    renderUi();
+  } catch (err) {
+    console.error(err);
+    return showError(err.message);
+  }
+};
 
 export { handleResetBtn };
